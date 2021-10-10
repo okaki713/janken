944389622b1ab50f2d5d8df66cbb280396ac7fe0
okaki713 <e1b19014@oit.ac.jp>
2021-10-11 04:42:34 +0900

    lec02ページの作成 janken機能の作成

--- src/main/java/oit/is/z1246/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1246/kaizi/janken/controller/Lec02Controller.java
@@ -25,4 +25,40 @@ public class Lec02Controller {
   public String lec021(){
     return "lec02.html";
   }
+
+
+  /**
+   * パスパラメータ2つをGETで受け付ける 1つ目の変数をparam1という名前で，2つ目の変数をparam2という名前で受け取る
+   * GETで受け取った2つの変数とsample22()の引数の名前が同じなため， 引数の前に @PathVariable と付けるだけで，パスパラメータの値を
+   * javaで処理できるようになる ModelMapはthymeleafに渡すためのMapと呼ばれるデータ構造を持つ変数
+   * Mapはkeyとvalueの組み合わせで値を保持する
+   *
+   * @param param1
+   * @param model
+   * @return
+   */
+  @GetMapping("/lec02/{param1}")
+  public String lec023(@PathVariable String param1, ModelMap model) {
+    String myhand;
+    String yourhand = "gu";
+    String judge;
+    if(param1.equals("gu")){
+      myhand = "gu";
+      judge = "Draw...";
+    }else if(param1.equals("choki")){
+      myhand = "choki";
+      judge = "You Lose...";
+    }else if(param1.equals("pa")){
+      myhand = "pa";
+      judge = "You Win!!";
+    }else{
+      myhand = "";
+      judge = "";
+    }
+    model.addAttribute("myhand", myhand);
+    model.addAttribute("yourhand", yourhand);
+    model.addAttribute("judge", judge);
+    return "lec02.html";
+
+  }
 }
